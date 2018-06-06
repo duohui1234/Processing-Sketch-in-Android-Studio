413d26d18c64861f08a2e42abad26ef8accf527e
정다혜 <duohui12@gmail.com>
2018-06-06 13:11:32 +0900

    Update processing sketch in android studio

--- Android_Processing/app/src/main/java/com/duohui/example/android_processing/MainActivity.java
+++ Android_Processing/app/src/main/java/com/duohui/example/android_processing/MainActivity.java
@@ -19,8 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Sketch sketch;
     TimerTask timerTask,timerTask2;
-    int cnt =40;
-    float degree = 100;
+    int cnt = 30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,17 +32,30 @@ public class MainActivity extends AppCompatActivity {
         PFragment fragment = new PFragment(sketch);
         fragment.setView(frameLayout, this);
 
-        timerTask = new TimerTask() {
-            @Override
-            public void run() {
-                sketch.setSpeed(cnt);
-                cnt++;
 
+
+
+
+
+        final int speed[] = new int[]{50,70,120,150,130,160,190,160,80,100,110,140,130,150,100,130,150};
+
+
+       timerTask = new TimerTask() {
+
+           int i =0;
+
+           @Override
+            public void run() {
+                sketch.setSpeed(speed[i]);
+
+                 i++;
+                 if(i == speed.length) timerTask.cancel();
             }
         };
 
         Timer timer = new Timer();
-        timer.schedule(timerTask,0,3000);
+        timer.schedule(timerTask,0,1000);
+
 
 
 
--- Android_Processing/app/src/main/java/com/duohui/example/android_processing/Sketch.java
+++ Android_Processing/app/src/main/java/com/duohui/example/android_processing/Sketch.java
@@ -7,88 +7,189 @@ import processing.core.PImage;
 
 public class Sketch extends PApplet {
 
-    PImage img1,img2,img3;
-    int speed=0;
-    float degree=0;
+    PImage img1, img2, img3;
+
+    int startSpeed = 0;
+    int endSpeed = 0;
+
+    int startDegree = 0;
+    int endDegree = 0;
+
+
+    int text;
+    int degree;
+    int arc;
+
+    boolean start = true;
+
+    int delaySecond =0;
 
 
 
 
-    public void settings()
-    {
-
+    public void settings() {
         fullScreen();
     }
 
-    public void setup(){
+    public void setup() {
 
-        background(255,255,255);
+
+        background(255, 255, 255);
         noStroke();
+
         img1 = loadImage("back.png");
+        img2 = loadImage("image.png");
+        img3 = loadImage("gra_img.png");
+
+
         imageMode(CENTER);
-        image(img1,width/2,height/2,920,920);
+        image(img1, width / 2, height / 2, 920, 920);
+
+        frameRate(240);
 
     }
 
-    public void setSpeed(int speed){
 
-        this.speed = speed;
+    public void setSpeed(int speed) {
+
+        delaySecond = 0;
+        frameCount = 0;
+
+        if (start) {
+
+            startSpeed = 0;
+            endSpeed = speed;
+
+            startDegree =0;
+            endDegree = speed*2;
+
+            start = false;
+
+        } else {
+
+            startSpeed = endSpeed;
+            endSpeed = speed;
+
+            startDegree = endDegree;
+            endDegree = speed*2;
+
+        }
+
+        text = startSpeed;
+        degree = startDegree;
+        arc = startDegree;
+
 
     }
 
-    public void setDegree(float degree){
 
-        this.degree = degree;
-    }
+    int i=1;
+    public void draw() {
+
+        imageMode(CENTER);
+
+        image(img1, width / 2, height / 2, 920, 920);
+
+
+        fill(120, 199, 184);
+        //각도 증가 제어
+        if (startDegree <= endDegree) {
+            arc = degree + 2;
+            arc(width / 2, height / 2, 910, 910, radians(130),radians((arc*2+90)/2+40));
 
 
 
-    public void draw(){
+        }
 
+        //각도 감소 제어
+        else if (startDegree > endDegree) {
+            arc = degree - 2;
+            arc(width / 2, height / 2, 910, 910, radians(130),radians((arc*2+90)/2+40));
 
-       fill(120,199,184);
-        arc(width/2, height/2, 910, 910, radians(130),radians(degree+88));
+        }
 
 
         //main image
-        img2 = loadImage("image.png");
-        imageMode(CENTER);
-        image(img2,width/2,height/2,880,880);
+        image(img2, width / 2, height / 2, 880, 880);
+
 
 
         //draw rect
         pushMatrix();
-        translate(width/2,height/2);
+        translate(width / 2, height / 2);
+
+
         rotate(radians(degree));
-        fill(183,194,199);
-        rect(-10,0,20,440);
+
+
+
+        fill(183, 194, 199, 200);
+        rect(-10, 0, 20, 440);
         popMatrix();
 
 
         //gradient image
-        img3 = loadImage("gra_img.png");
-        imageMode(CENTER);
-        image(img3,width/2,height/2,300,300);
+        image(img3, width / 2, height / 2, 300, 300);
 
 
         //text
         pushMatrix();
-        translate(width/2,height/2);
+        translate(width / 2, height / 2);
         textAlign(CENTER);
 
-        fill(255,255,255);
+        fill(255, 255, 255);
         textSize(135);
-        text(speed,0,0);
 
-        fill(255,255,255,100);
+
+        text(text, 0, 0);
+
+
+        fill(255, 255, 255, 100);
         textSize(35);
-        text("km/h",0,80);
+        text("km/h", 0, 80);
 
         popMatrix();
+
+
+        //텍스트 증가제어
+        if (startSpeed <= endSpeed && text <= endSpeed - 1) {
+            text = text+1 ;
+        }
+
+        //텍스트 감소제어
+        else if (startSpeed > endSpeed && text >= endSpeed + 1) {
+            text=text-1;
+        }
+
+
+
+        //각도 증가 제어
+        if (startDegree <= endDegree && degree <= endDegree - 1) {
+             degree = degree + 2;
+            //controllPlusDegree(abs(endSpeed-startSpeed));
+
+
+        }
+
+        //각도 감소 제어
+        else if (startDegree > endDegree && degree >= endDegree + 1 ) {
+             degree = degree - 2;
+            //controllMinusDegree(abs(endSpeed-startSpeed));
+        }
+
+
+
+
+        if(frameCount > abs(abs(endSpeed-startSpeed)-30)){
+                  delaySecond = delaySecond + 1;
+                  delay(delaySecond);
+        }
 
 
 
 
     }
+
+
 
 }
